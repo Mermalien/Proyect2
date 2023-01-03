d38a9c5f4735ce9f5dc5ad4085faed996b077497
Flor <mflor78@hotmail.com>
2023-01-03 13:46:25 +0100

    mas cosas

--- src/controllers/posts/editPost.js
+++ src/controllers/posts/editPost.js
@@ -2,17 +2,34 @@ const { selectPostById, updatePostById } = require("../../repositories/posts");
 const generateError = require("../../utils/generateError");
 
 const editPost = async (req, res, next) => {
-    try {
-        const { id } = req.params;
+  try {
+    const { id } = req.params;
 
-        const post = await selectPostById(id);
+    const post = await selectPostById(id);
 
-        if (!post) {
-            generateError("El post que quieres actualizar, no existe!" , 404);
+    if (!post) {
+      generateError("El post que quieres actualizar, no existe!", 404);
 
-            const loggue
-        }
-    } catch (error) {
-        
+      const logguedUserId = req.auth.id;
+
+      if (post.userId !== logguedUserId) {
+        generateError("No tienes permiso para edit este Post", 401);
+      }
+      const { title, url, descripcion } = req.body;
+
+      if (!title && !url && !descripcion) {
+        generateError(
+          "Necesitas incluir un título, una url y una descripcion",
+          400
+        );
+      }
+
+      const updatedPost = { ...post, ...req.body };
+      await updatePostById(updatedPost);
+      res.status(200).send({ status: "ok", data: updatedPost });
     }
-}
+  } catch (error) {
+    next(error);
+  }
+};
+module.exports = editPost;
